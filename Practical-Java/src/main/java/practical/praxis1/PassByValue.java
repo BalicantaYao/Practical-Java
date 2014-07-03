f3215bb14b3ddd45b4a0a34b7330de4d948e0f3e
balicanta <balicanta.yao@gmail.com>
2014-07-04 01:23:45 +0800

    Add Logback config

--- Practical-Java/src/main/java/practical/praxis1/PassByValue.java
+++ Practical-Java/src/main/java/practical/praxis1/PassByValue.java
@@ -1,9 +1,14 @@
 package practical.praxis1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class PassByValue {
+    private static Logger logger = LoggerFactory.getLogger(PassByValue.class);
 
 	public static void main(String[] args) {
-		
+		logger.debug("Hey");
 	}
 
 }
